2b3a2f3400c76dc54e939425dd01c2512ca5b6b2
Grigoriy Ostrovskiy <greevex@gmail.com>
2015-10-28 20:44:46 +0300

    add simpleConnector class to connect some sockets

---
+++ src/server/simpleConnector.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace greevex\react\easyServer\server;
+
+use React;
+
+/**
+ * Class simpleConnector
+ *
+ * @package greevex\react\easyServer\server
+ * @author Gregory Ostrovsky <greevex@gmail.com>
+ */
+class simpleConnector
+{
+
+    /**
+     * @var React\EventLoop\LoopInterface
+     */
+    private $loop;
+
+    /**
+     * @var array
+     */
+    private $config = [
+        'dns' => '8.8.8.8',
+    ];
+
+    /**
+     * @var React\SocketClient\Connector
+     */
+    protected $connector;
+
+    /**
+     * @param React\EventLoop\LoopInterface $loop
+     * @param array                         $config
+     */
+    public function __construct(React\EventLoop\LoopInterface $loop, array $config = null)
+    {
+        $this->loop = $loop;
+        if($config !== null) {
+            $this->config = $config;
+        }
+    }
+
+    /**
+     * @return React\SocketClient\Connector
+     */
+    protected function getConnector()
+    {
+        if($this->connector === null) {
+            $dnsResolverFactory = new React\Dns\Resolver\Factory();
+            $dns = $dnsResolverFactory->createCached($this->config['dns'], $this->loop);
+            $this->connector = new React\SocketClient\Connector($this->loop, $dns);
+        }
+
+        return $this->connector;
+    }
+}
