4974abd420ac24c970920886f55b1465352cd254
Grigoriy Ostrovskiy <greevex@gmail.com>
2015-10-28 19:43:37 +0300

    communication: add pingPongCommunication for example

---
+++ src/communication/pingPongCommunication.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace greevex\react\easyServer\communication;
+
+/**
+ * Abstract communication scenario
+ *
+ * Class describes structured communication between client and server
+ *
+ * @package greevex\react\easyServer\communication
+ * @author Gregory Ostrovsky <greevex@gmail.com>
+ */
+class pingPongCommunication
+    extends abstractCommunication
+{
+
+    /**
+     * Process new received client command
+     *
+     * @param $command
+     */
+    protected function clientCommand($command)
+    {
+        switch($command['request']) {
+            case 'ping':
+                $time = microtime(true);
+                $writeLatency = $time - $command['payload']['time'];
+                error_log('Write latency: ' . ((int)$writeLatency*1000) . 'ms');
+                $this->client->send([
+                    'request' => 'pong',
+                    'payload' => [
+                        'time' => $time,
+                        'latency' => $writeLatency,
+                    ],
+                ]);
+                break;
+            case 'pong':
+                $time = microtime(true);
+                $readLatency = $time - $command['payload']['time'];
+                error_log('Read latency: ' . ((int)$readLatency*1000) . 'ms');
+                $this->client->send([
+                    'request' => 'ping',
+                    'payload' => [
+                        'time' => $time,
+                        'latency' => $readLatency,
+                    ],
+                ]);
+                break;
+            case 'getTime':
+                $time = microtime(true);
+                $this->client->send([
+                    'request' => 'time',
+                    'payload' => [
+                        'unixtime' => $time,
+                        'w3c' => date(DATE_W3C, $time),
+                    ],
+                ]);
+                break;
+            default:
+                error_log('Unknown command');
+                $this->client->send([
+                    'request' => 'unknown_command',
+                ]);
+                break;
+        }
+    }
+}
